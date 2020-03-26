ca58d77c3e123da091a165cbda69ac46663f3a1f
DimitrisKapoulas <kapoulasd@gmail.com>
2020-03-26 23:48:13 +0200

    Some more styling + A new field displayed on the html

--- src/main/java/com/quarantine/coronavirustracker/controllers/HomeController.java
+++ src/main/java/com/quarantine/coronavirustracker/controllers/HomeController.java
@@ -20,8 +20,10 @@ public class HomeController {
     public String home(Model model) {
         List<LocationStats> allStats = coronaVirusDataService.getAllStats();
         int totalCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+        int totalNewCases = allStats.stream().mapToInt(stat -> stat.getDiffFromPreviousDay()).sum();
         model.addAttribute("locationStats", allStats);
         model.addAttribute("totalReportedCases", totalCases);
+        model.addAttribute("totalNewCases",totalNewCases);
 
         return "home";
     }
